136cf4368cd3c9117b9da36b9160997ae84e55d2
anuujj <anujk8097@gmail.com>
2024-02-22 19:34:20 +0530

    about page css

--- src/Components/Icard.tsx
+++ src/Components/Icard.tsx
@@ -1,13 +1,13 @@
 import Handles from "./Handles";
 import Shortcut from "./Shortcut"
+import "../styles/icard.css"
 
 export default function Icard() {
   return (
     <div className="icard">
-      <div>
-        <img className="pfp" src="" />
-      </div>
+      <div className="avatar"></div>
       <h1 className="name">Anuj</h1>
+      <div className="ruler"></div>
       <h2 className="designation">Software Engineer</h2>
       <Shortcut className="fromIdCard"/>
       <Handles/>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,24 +1,29 @@
 import Icard from "../Components/Icard";
 import Shortcut from "../Components/Shortcut";
+import "../styles/about.css";
 
 export default function About() {
   return (
-    <div>
-      <Icard />
-      <section>
-        <h1>Hello 👋</h1>
-        <Shortcut className="fromAbout"/>
-        <article>
-          Anuj is currently working as a Software Developer at Paytm, a leading
-          fintech company in India. With over 1.5+ years of experience in
-          frontend development, Anuj possesses a robust skill set that includes
-          expertise in HTML, CSS, JavaScript/TypeScript, and proficiency in
-          frameworks such as React. His work experience extends to an agile
-          environment, where he has demonstrated his abilities in adapting to
-          dynamic development processes. He is well-versed in CI/CD pipelines.
-          He has experience in building robust web pages for scale.
-        </article>
-      </section>
+    <div className="about">
+      <div className="icardContainer">
+        <Icard />
+      </div>
+      <div className="sectionContainer">
+        <section>
+          <h1>Hello 👋</h1>
+          <Shortcut className="fromAbout" />
+          <article>
+            I am currently working as a Software Developer at Paytm, a leading
+            fintech company in India. With over 1.5+ years of experience in
+            frontend development, I possess a robust skill set that includes
+            expertise in HTML, CSS, JavaScript/TypeScript, and proficiency in
+            frameworks such as React. My work experience extends to an agile
+            environment, where I have demonstrated my abilities in adapting to
+            dynamic development processes. I am well-versed in CI/CD pipelines
+            and have experience in building robust web pages for scale.
+          </article>
+        </section>
+      </div>
     </div>
   );
 }
